a2ea790dbe1fc5dea0d7c50b2e99d4aaaeabfa0c
gopi <gopi2320101at1996@gmail.com>
2024-03-22 11:41:49 +0530

    second commit

---
+++ casestudy_AI/main.py
@@ -0,0 +1,49 @@
+import uvicorn
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import pickle
+import pandas as pd
+
+class BankNote(BaseModel):
+    Variance:float
+    skewness:float
+    curtosis:float
+    entropy:float
+
+app=FastAPI()
+pickle_in=open("classifier.pkl","rb")
+classifier=pickle.load(pickle_in)
+
+@app.get('/')
+def index():
+    return {"message":"hello stranger"}
+
+@app.get('/{name}')
+def get_name(name:str):
+    return {"message":"f'{name}"}
+@app.post('/predict')
+def BankNote_Prediction(data:BankNote):
+     data=data.dict()
+     print(data)
+     print("hello")
+     Variance=data['Variance']
+     skewness=data['skewness']
+     curtosis=data['curtosis']
+     entropy=data['entropy']
+     prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+     if prediction[0] >= 0.5:
+         prediction ="fake note"
+     else:
+         prediction ="Bank note"
+     return{
+        'prediction': prediction 
+        }
+
+
+if __name__=='__main__':
+    uvicorn.run(app ,host='127.0.0.1',port=8000)
+  
+
+
+
